e8aacb7f2e63ae1221c47c680a7b62a533f33484
Emmanuel Delobre <emmanueldelobre@MacBook-Pro-Avril-2019.local>
2020-12-27 22:27:25 +0100

    Section 4: JDBC dans les applications - 13 La classe Datasource V1

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -5,15 +5,23 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class TestDeConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
+        	MysqlDataSource dataSource = new MysqlDataSource();
+        	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+        	dataSource.setUser("DBCOURS");
+        	dataSource.setPassword("DBCOURS");
+        	
+        	conn = dataSource.getConnection();
             //Seulement avant Java 7/JDBC 4 
             //Class.forName(DRIVER_CLASS_NAME);
             
             //MySQL driver MySQL Connector
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","DBCOURS","DBCOURS");
+           // conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","manu","xevrod2x");
             conn.setAutoCommit(false);
             PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM, PRENOM, SEXE) VALUES (?, ?, ?)");
            
