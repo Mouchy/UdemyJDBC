c8d3356ce150b8c0ba173ec4ae819fcd5da83a2f
Emmanuel Delobre <emmanueldelobre@MacBook-Pro-Avril-2019.local>
2020-12-27 20:04:42 +0100

    Section 3: Opérations de base JDBC - 11 Ecrire dans la base de données

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -3,9 +3,7 @@ package com.mycompany.tennis.core;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestDeConnection {
     public static void main(String... args){
@@ -15,23 +13,18 @@ public class TestDeConnection {
             //Class.forName(DRIVER_CLASS_NAME);
             
             //MySQL driver MySQL Connector
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","manu","xevrod2x");
-            PreparedStatement preparedStatement = conn.prepareStatement("SELECT NOM, PRENOM, ID FROM JOUEUR WHERE ID = ?");
-            long identifiant  = 43L;
-            preparedStatement.setLong(1, identifiant);
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","DBCOURS","DBCOURS");
+            PreparedStatement preparedStatement = conn.prepareStatement("update JOUEUR SET NOM = ?, PRENOM = ? WHERE ID = ?");
+            long identifiant  = 24L;
+            String nom = "Errani";
+            String prenom = "Sara";
+            preparedStatement.setString(1, nom);
+            preparedStatement.setString(2, prenom);
+            preparedStatement.setLong(3, identifiant);
            
-            ResultSet rs = preparedStatement.executeQuery();
+            int nbEnregistrementsModifies = preparedStatement.executeUpdate();
             
-            if (rs.next()) {
-                final String nom = rs.getString("NOM");
-                final String prenom = rs.getString("PRENOM");
-                final Long id = rs.getLong("ID");
-                System.out.println("Le joueur  / la joueuse représenté par le numéro " +id+" est "+prenom+" "+nom);
-            }
-            else {
-            	System.out.println("Il n'y a pas d'enregistrement d'Id 12");
-            }
-            
+            System.out.println("Nombre d'enregistrements modifiés="+nbEnregistrementsModifies);
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
