fd76eeef5784a369864be3da9f3ce726f8e24e83
Emmanuel Delobre <emmanueldelobre@MacBook-Pro-Avril-2019.local>
2020-12-28 09:47:40 +0100

    Section 4: JDBC dans les applications - 16 Premiere implementation de Repository

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -1,71 +1,8 @@
 package com.mycompany.tennis.core;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import org.apache.commons.dbcp2.BasicDataSource;
-
-
 public class TestDeConnection {
     public static void main(String... args){
-        Connection conn = null;
-        try {
-        	BasicDataSource dataSource = new BasicDataSource();
-        	dataSource.setInitialSize(5);
-        	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
-        	
-        	dataSource.setUsername("DBCOURS");
-        	dataSource.setPassword("DBCOURS");
-        	
-        	conn = dataSource.getConnection();
-            //Seulement avant Java 7/JDBC 4 
-            //Class.forName(DRIVER_CLASS_NAME);
-            
-            //MySQL driver MySQL Connector
-           // conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","manu","xevrod2x");
-            conn.setAutoCommit(false);
-            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM, PRENOM, SEXE) VALUES (?, ?, ?)");
-           
-            String nom    = "Capriati";
-            String prenom = "Jennifer";
-            String sexe   = "F";
-            preparedStatement.setString(1, nom);
-            preparedStatement.setString(2, prenom);
-            preparedStatement.setString(3, sexe);
-           
-            preparedStatement.executeUpdate();
-            
-            nom    = "Johannson";
-            prenom = "Thomas";
-            sexe   = "H";
-            preparedStatement.setString(1, nom);
-            preparedStatement.setString(2, prenom);
-            preparedStatement.setString(3, sexe);
-           
-            preparedStatement.executeUpdate();
-            
-            conn.commit();
-            
-
-            System.out.println("success");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            try {
-            	if (conn!=null) conn.rollback();
-            } catch (SQLException e1) {
-                e.printStackTrace();
-            }
-        }
-        finally {
-            try {
-                if (conn!=null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+       
     }
 }
 
--- src/main/java/com/mycompany/tennis/core/repository/JoueurRepositoryImpl.java
+++ src/main/java/com/mycompany/tennis/core/repository/JoueurRepositoryImpl.java
@@ -1,5 +1,229 @@
 package com.mycompany.tennis.core.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import com.mycompany.tennis.core.entity.Joueur;
+
 public class JoueurRepositoryImpl {
 
+	public void create(Joueur joueur) {
+		Connection conn = null;
+        try {
+        	BasicDataSource dataSource = new BasicDataSource();
+        	dataSource.setInitialSize(5);
+        	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+        	
+        	dataSource.setUsername("DBCOURS");
+        	dataSource.setPassword("DBCOURS");
+        	
+        	conn = dataSource.getConnection();
+
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM, PRENOM, SEXE) VALUES (?, ?, ?)");
+
+            preparedStatement.setString(1, joueur.getNom());
+            preparedStatement.setString(2, joueur.getPrenom());
+            preparedStatement.setString(3, joueur.getSexe().toString());
+           
+            preparedStatement.executeUpdate();
+            
+            System.out.println("Joueur créé");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+            	if (conn!=null) conn.rollback();
+            } catch (SQLException e1) {
+                e.printStackTrace();
+            }
+        }
+        finally {
+            try {
+                if (conn!=null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+	
+	public void update(Joueur joueur) {
+		Connection conn = null;
+	    try {
+	    	BasicDataSource dataSource = new BasicDataSource();
+	    	dataSource.setInitialSize(5);
+	    	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+	    	
+	    	dataSource.setUsername("DBCOURS");
+	    	dataSource.setPassword("DBCOURS");
+	    	
+	    	conn = dataSource.getConnection();
+	
+	        PreparedStatement preparedStatement = conn.prepareStatement("UPDATE JOUEUR set NOM=?, PRENOM=?, SEXE=? where ID = ?");
+	
+	        preparedStatement.setString(1, joueur.getNom());
+	        preparedStatement.setString(2, joueur.getPrenom());
+	        preparedStatement.setString(3, joueur.getSexe().toString());
+	        preparedStatement.setLong(4, joueur.getId());
+	       
+	        preparedStatement.executeUpdate();
+	        
+	        System.out.println("Joueur modifié");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        try {
+	        	if (conn!=null) conn.rollback();
+	        } catch (SQLException e1) {
+	            e.printStackTrace();
+	        }
+	    }
+	    finally {
+	        try {
+	            if (conn!=null) {
+	                conn.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+	public void delete(Long id) {
+		Connection conn = null;
+	    try {
+	    	BasicDataSource dataSource = new BasicDataSource();
+	    	dataSource.setInitialSize(5);
+	    	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+	    	
+	    	dataSource.setUsername("DBCOURS");
+	    	dataSource.setPassword("DBCOURS");
+	    	
+	    	conn = dataSource.getConnection();
+	
+	        PreparedStatement preparedStatement = conn.prepareStatement("DELETE FROM JOUEUR WHERE ID = ?");
+	
+	        preparedStatement.setLong(1, id);
+	       
+	        preparedStatement.executeUpdate();
+	        
+	        System.out.println("Joueur suprimé");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        try {
+	        	if (conn!=null) conn.rollback();
+	        } catch (SQLException e1) {
+	            e.printStackTrace();
+	        }
+	    }
+	    finally {
+	        try {
+	            if (conn!=null) {
+	                conn.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+	public Joueur getById(Long id) {
+		Connection conn = null;
+		Joueur joueur = null;
+	    try {
+	    	BasicDataSource dataSource = new BasicDataSource();
+	    	dataSource.setInitialSize(5);
+	    	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+	    	
+	    	dataSource.setUsername("DBCOURS");
+	    	dataSource.setPassword("DBCOURS");
+	    	
+	    	conn = dataSource.getConnection();
+	
+	        PreparedStatement preparedStatement = conn.prepareStatement("SELECT NOM, PRENOM, SEXE FROM JOUEUR WHERE ID = ?");
+	
+	        preparedStatement.setLong(1, id);
+	       
+	        ResultSet rs = preparedStatement.executeQuery();
+	        
+	        if (rs.next()) {
+	        	joueur = new Joueur();
+	        	joueur.setId(id);
+	        	joueur.setNom(rs.getString("NOM"));
+	        	joueur.setPrenom(rs.getString("PRENOM"));
+	        	joueur.setSexe(rs.getString("SEXE").charAt(0));
+	        }
+	        
+	        System.out.println("Joueur lu");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        try {
+	        	if (conn!=null) conn.rollback();
+	        } catch (SQLException e1) {
+	            e.printStackTrace();
+	        }
+	    }
+	    finally {
+	        try {
+	            if (conn!=null) {
+	                conn.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return joueur;
+	}
+	
+	public List<Joueur> list() {
+		Connection conn = null;
+		List<Joueur> joueurs = new ArrayList<>();
+	    try {
+	    	BasicDataSource dataSource = new BasicDataSource();
+	    	dataSource.setInitialSize(5);
+	    	dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+	    	
+	    	dataSource.setUsername("DBCOURS");
+	    	dataSource.setPassword("DBCOURS");
+	    	
+	    	conn = dataSource.getConnection();
+	
+	        PreparedStatement preparedStatement = conn.prepareStatement("SELECT ID, NOM, PRENOM, SEXE FROM JOUEUR");
+	       
+	        ResultSet rs = preparedStatement.executeQuery();
+	        
+	        while (rs.next()) {
+	        	Joueur joueur = new Joueur();
+	        	joueur.setId(rs.getLong("ID"));
+	        	joueur.setNom(rs.getString("NOM"));
+	        	joueur.setPrenom(rs.getString("PRENOM"));
+	        	joueur.setSexe(rs.getString("SEXE").charAt(0));
+	        	joueurs.add(joueur);
+	        }
+	        
+	        System.out.println("Joueurs lus");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        try {
+	        	if (conn!=null) conn.rollback();
+	        } catch (SQLException e1) {
+	            e.printStackTrace();
+	        }
+	    }
+	    finally {
+	        try {
+	            if (conn!=null) {
+	                conn.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return joueurs;
+	}
 }
