5902cbc38e4559949288f709ae8acd6345ff2d41
Emmanuel Delobre <emmanueldelobre@MacBook-Pro-Avril-2019.local>
2021-01-08 17:11:43 +0100

    Section7 Hibernate ce qu'il faut savoir pour bien demarrer 36 Le mapping des colonnes en detail

--- src/main/java/com/mycompany/tennis/core/entity/Epreuve.java
+++ src/main/java/com/mycompany/tennis/core/entity/Epreuve.java
@@ -1,9 +1,15 @@
 package com.mycompany.tennis.core.entity;
 
+import javax.persistence.Column;
+
+import org.hibernate.annotations.Type;
+
 public class Epreuve {
 	private Long      id;
+	@Type(type="short")
 	private short     annee;
 	private Tournoi   tournoi;
+	@Column(name="TYPE_EPREUVE", nullable = true, length=2000)
 	private Character typeEpreuve;
 	
 	public Long getId() {
